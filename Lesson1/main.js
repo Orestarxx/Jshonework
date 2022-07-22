9fdc9e7712d17b96c5897c533865978b8d974630
Orestarxx <oresto322@gmail.com>
2022-07-22 19:31:35 +0300

    Initial commit

---
+++ Lesson1/main.js
@@ -0,0 +1,101 @@
+let a =`Hello`;
+console.log(a);
+alert(a);
+document.write (a);
+
+let b =`owu`;
+console.log(b);
+alert(b);
+document.write(b);
+
+let c =`ua`;
+console.log(c);
+alert(c);
+document.write(c);
+
+let d =`com`;
+console.log(d);
+alert(d);
+document.write(d);
+
+let aa = 17;
+let bb = 2;
+let res = aa % bb;
+console.log(res);
+alert(res);
+document.write(res);
+
+let cc = -1000 + true;
+console.log(cc);
+alert(cc);
+document.write(cc);
+
+let one = true;
+console.log(one);
+alert(one);
+document.write(one);
+
+
+let two = false;
+console.log(two);
+alert(two);
+document.write(two);
+
+let one1 = 20-10;
+console.log(one1);
+alert(one1);
+document.write(one1);
+
+let one2 = 3.14 ;
+console.log(one2);
+alert(one2);
+document.write(one2);
+
+let one3 =2.7;
+console.log(one3);
+alert(one3);
+document.write(one3);
+
+let twoo =4 * 4;
+console.log(twoo);
+alert(twoo);
+document.write(twoo);
+
+
+let aaa = 100;
+console.log(typeof aaa);
+let bbb = '100';
+console.log(typeof bbb);
+let ccc = true;
+console.log(typeof ccc);
+
+
+
+
+let firstname = `Orest`;
+let middlename = `Hanes`;
+let lastname = `Jaroslavovich`;
+let space =` `;
+let person = firstname + space + middlename + space + lastname;
+console.log(person);
+
+
+
+
+let name = prompt();
+let middlenamee = prompt();
+let age = prompt();
+console.log(name,middlenamee,age);
+
+
+
+
+
+
+
+
+
+
+
+
+
