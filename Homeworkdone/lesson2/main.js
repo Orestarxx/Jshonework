27a743d400c9ad8cbaa6687133b217f0d5e51988
Orestarxx <oresto322@gmail.com>
2022-07-27 13:50:50 +0300

    newwwww

---
+++ Homeworkdone/lesson2/main.js
@@ -0,0 +1,102 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let friends = [`John`,`Petro`,`Vasyl`,`Ivan`,`Oleg`,`Maria`,`Nadia`,`Ann`,`Rick`,`Morty`];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+console.log(friends[4]);
+console.log(friends[5]);
+console.log(friends[6]);
+console.log(friends[7]);
+console.log(friends[8]);
+console.log(friends[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об`єкту : title ,pageCount, genre.
+
+let book1 ={
+    title:`Ripka`,
+    pagecount:24,
+    genre:'for children'
+};
+console.log(book1);
+let book2 ={
+    title:`It`,
+    pagecount:1344,
+    genre:'Horror'
+};
+console.log(book2);
+let book3 ={
+    title:`Sherlock Holmes`,
+    pagecount:592,
+    genre:'detective'
+};
+console.log(book3);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+
+let book4 ={
+    title:`it`,
+    pagecount:1344,
+    genre:`horror`,
+    autors:[
+        {
+            name:`Petro`,
+            age:45
+        }
+    ]
+};
+console.log(book4);
+let book5 ={
+    title:`Ripka`,
+    pagecount:24,
+    genre:`For children`,
+    autors:[
+        {
+            name:`Ivan`,
+            age:35
+        }
+    ]
+};
+console.log(book5)
+let book6 ={
+    title:`Sherlock Holmes`,
+    pagecount:592,
+    genre:`detective`,
+    autors:[
+        {
+            name:`Taras`,
+            age:55
+        }
+    ]
+};
+console.log(book6)
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let Users = [
+    { name:`Diana`, username:`pickle` , password:2233311},
+    { name:`Petro`, username:`Egg` , password:12412121555 },
+    { name:`Ivan`, username:`Apple` , password:343425555555 },
+    { name:`Dayana`, username:`Dish` , password:43626346363646},
+    { name:`Rick`, username:`Ricky` , password:1111},
+    { name:`Morty`, username:`Gun`, password:51112222222 },
+    { name:`Ann`, username:`Andromeda`, password:63255323333 },
+    { name:`Oleg`, username:`ogirok`, password:756464525235 },
+    { name: `Nadia` , username:`Pineapple`, password:845346366346346 },
+    { name:`Maria`, username:`Milk`, password:9565464326236236634 }
+];
+console.log(Users[0][`password`])
+console.log(Users[1][`password`])
+console.log(Users[2][`password`])
+console.log(Users[3][`password`])
+console.log(Users[4][`password`])
+console.log(Users[5].password)
+console.log(Users[6].password)
+console.log(Users[7].password)
+console.log(Users[8].password)
+console.log(Users[9].password)
+
+
+
